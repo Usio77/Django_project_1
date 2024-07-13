a7d2be815498e373ef65ac117ce3f5a67b150369
unknown <shrestha7prafulla@gmail.com>
2024-07-13 15:25:53 +0545

    Comment

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from blog.models import Category
 
 # Create your views here.
 def home(request):
+    categories= Category.objects.all()  # retrieves all the records from the Category table and stores them in the categories.
+    context = {'cate':categories} # Creates a dictionary to pass the QuerySet to the template.
+    # here 'cate' is a string key which will be used in templates to access the associate data.
     return render(request,'base.html')
